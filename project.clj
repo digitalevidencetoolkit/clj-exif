5b9489a7188209e203bd2b8fea18d2478fa298c0
Basile Simon <basile@basilesimon.fr>
2021-03-13 17:36:31 +0100

    Bump Clojure version, HTTPS Apache requirements

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/olimcc/clj-exif"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.commons/commons-imaging "1.0-SNAPSHOT"]]
   :repositories [
-                 ["apache.snapshots" "http://repository.apache.org/snapshots"]])
+                 ["apache.snapshots" "https://repository.apache.org/snapshots"]])
 
 
