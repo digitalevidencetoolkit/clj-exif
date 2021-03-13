15be28ffbd87076fddb0f0808de9811b16a54bee
Basile Simon <basile@basilesimon.fr>
2021-03-13 17:42:43 +0100

    Write a series of metadata sets to file

--- src/clj_exif/examples.clj
+++ src/clj_exif/examples.clj
@@ -2,6 +2,7 @@
   "Examples usage of the exif utilities."
   (:require [clojure.java.io :as jio]
             [clojure.pprint :as pprint]
+            [clojure.set :as set]
             [clj-exif.core :as exif])
   (:import
     [java.io File]
@@ -12,7 +13,7 @@
     [org.apache.commons.imaging.common RationalNumber]))
 
 ;; read data
-(let [input-file (jio/as-file (jio/resource "pic.geo.jpg"))
+(let [input-file (jio/as-file (jio/resource "gps.JPG"))
       metadata (exif/get-metadata input-file)]
   (println (pprint/pprint (exif/read metadata))))
 
@@ -32,30 +33,28 @@
                      ["this is a new value"])
   (exif/copy-file-with-new-metadata input-file output-file output-set))
 
-;; write a new property, 'Artist'
-(let [input-file (jio/as-file (jio/resource "pic.geo.jpg"))
-      output-file (File. "/tmp/output.jpg")
-      metadata (exif/get-metadata input-file)
-      ;; output-set is a writeable copy of the data retrieved from input-file.
-      output-set (exif/get-output-set metadata)]
+(let [input-file   (jio/as-file (jio/resource "gps.JPG"))
+      output-file  (jio/as-file (jio/resource "out.jpg"))
+      metadata     (exif/get-metadata input-file)
+      output-set  (exif/get-output-set metadata)]
   (exif/update-value output-set
-                     TiffDirectoryConstants/DIRECTORY_TYPE_ROOT
-                     TiffTagConstants/TIFF_TAG_ARTIST
-                     ["Fancy Artist"])
-  (exif/copy-file-with-new-metadata input-file output-file output-set))
-
-;; update the longitude co-ordinate
-(let [input-file (jio/as-file (jio/resource "pic.geo.jpg"))
-      output-file (File. "/tmp/output.jpg")
-      metadata (exif/get-metadata input-file)
-      ;; output-set is a writeable copy of the data retrieved from input-file.
-      output-set (exif/get-output-set metadata)]
+                     TiffDirectoryConstants/DIRECTORY_TYPE_GPS
+                     GpsTagConstants/GPS_TAG_GPS_LATITUDE
+                     [(RationalNumber. 46 1)
+                      (RationalNumber. 22 1)
+                      (RationalNumber. 562 10)])
   (exif/update-value output-set
                      TiffDirectoryConstants/DIRECTORY_TYPE_GPS
                      GpsTagConstants/GPS_TAG_GPS_LONGITUDE
-                     [(RationalNumber. 11 1)
-                      (RationalNumber. 11 1)
-                      (RationalNumber. 32 100)])
+                     [(RationalNumber. 6 1)
+                      (RationalNumber. 14 1)
+                      (RationalNumber. 334 10)])
+  (exif/update-value output-set
+                     TiffDirectoryConstants/DIRECTORY_TYPE_ROOT
+                     TiffTagConstants/TIFF_TAG_MAKE
+                     ["Huawei"])
+  (exif/update-value output-set
+                     TiffDirectoryConstants/DIRECTORY_TYPE_ROOT
+                     TiffTagConstants/TIFF_TAG_MODEL
+                     ["P30 Pro back camera"])
   (exif/copy-file-with-new-metadata input-file output-file output-set))
-
-
